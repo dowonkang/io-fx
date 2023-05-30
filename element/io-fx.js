c312378c264c23eb2de0e03630dcbb8c5b5f46e4
Dowon Kang <dev@dowonkang.name>
2023-05-30 15:07:22 +0900

    some code samples in index.html

--- element/io-fx.js
+++ element/io-fx.js
@@ -1,5 +1,6 @@
-const template = document.createElement("template");
-template.innerHTML = `
+(function () {
+  const template = document.createElement("template");
+  template.innerHTML = `
   <style>
     :host {
       display: block;
@@ -144,157 +145,156 @@ template.innerHTML = `
   <div class="io-fx-target"><slot></slot></div>
 `;
 
-class IoFx extends HTMLElement {
-  /** @type {IntersectionObserver} */
-  observer;
+  class IoFxElement extends HTMLElement {
+    /** @type {IntersectionObserver} */
+    observer;
 
-  /** @type {Map<string, IntersectionObserver>} */
-  static observers = new Map();
+    /** @type {Map<string, IntersectionObserver>} */
+    static observers = new Map();
 
-  constructor() {
-    super();
+    constructor() {
+      super();
 
-    const shadowRoot = this.attachShadow({ mode: "open" });
-    shadowRoot.appendChild(template.content.cloneNode(true));
-  }
-
-  get name() {
-    return this.getAttribute("name");
-  }
-
-  get active() {
-    return this.hasAttribute("active");
-  }
-
-  set active(flag) {
-    if (flag) {
-      this.setAttribute("active", "");
-    } else {
-      this.removeAttribute("active");
-    }
-  }
-
-  get once() {
-    return this.hasAttribute("once");
-  }
-
-  get root() {
-    if (this.observer) {
-      return this.observer.root;
+      const shadowRoot = this.attachShadow({ mode: "open" });
+      shadowRoot.appendChild(template.content.cloneNode(true));
     }
 
-    const attrValue = this.getAttribute("root");
-    return attrValue ? document.querySelector(attrValue) : null;
-  }
-
-  get rootMargin() {
-    if (this.observer) {
-      return this.observer.rootMargin;
+    get name() {
+      return this.getAttribute("name");
     }
 
-    const attrValue = this.getAttribute("root-margin");
-    return attrValue || "0px 0px 0px 0px";
-  }
-
-  get thresholds() {
-    if (this.observer) {
-      return this.observer.thresholds;
+    get active() {
+      return this.hasAttribute("active");
     }
 
-    const attrValue = this.getAttribute("thresholds");
-    /** @type {number | number[]} */
-    let thresholds = 0.75;
-
-    if (attrValue) {
-      try {
-        const parsed = JSON.parse(attrValue);
-
-        if (typeof parsed === "number" || Array.isArray(parsed)) {
-          thresholds = parsed;
-        } else {
-          throw new Error("Invalid threshold value");
-        }
-      } catch (e) {}
+    set active(flag) {
+      if (flag) {
+        this.setAttribute("active", "");
+      } else {
+        this.removeAttribute("active");
+      }
     }
 
-    return Array.isArray(thresholds) ? thresholds : [thresholds];
-  }
-
-  get options() {
-    if (this.observer) {
-      const { root, rootMargin, thresholds: threshold } = this.observer;
-      return { root, rootMargin, threshold };
+    get once() {
+      return this.hasAttribute("once");
     }
 
-    const { root, rootMargin, thresholds: threshold } = this;
+    get root() {
+      if (this.observer) {
+        return this.observer.root;
+      }
 
-    return {
-      root,
-      rootMargin,
-      threshold,
-    };
-  }
+      const attrValue = this.getAttribute("root");
+      return attrValue ? document.querySelector(attrValue) : null;
+    }
 
-  connectedCallback() {
-    [
-      "duration",
-      "opacity-duration",
-      "transform-duration",
-      "delay",
-      "opacity-delay",
-      "transform-delay",
-      "easing",
-      "opacity-easing",
-      "transform-easing",
-    ].forEach((attr) => {
-      const attrValue = this.getAttribute(attr);
+    get rootMargin() {
+      if (this.observer) {
+        return this.observer.rootMargin;
+      }
+
+      const attrValue = this.getAttribute("root-margin");
+      return attrValue || "0px 0px 0px 0px";
+    }
+
+    get thresholds() {
+      if (this.observer) {
+        return this.observer.thresholds;
+      }
+
+      const attrValue = this.getAttribute("thresholds");
+      /** @type {number | number[]} */
+      let thresholds = 0.75;
 
       if (attrValue) {
-        this.style.setProperty(`--io-fx-${attr}`, attrValue);
-      }
-    });
+        try {
+          const parsed = JSON.parse(attrValue);
 
-    if (!this.observer) {
-      const { options } = this;
-      const observerKey = JSON.stringify(options);
-
-      if (!IoFx.observers.has(observerKey)) {
-        const observer = new IntersectionObserver((entries, observer) => {
-          entries.forEach((entry) => {
-            const target = entry.target;
-
-            if (entry.isIntersecting) {
-              target.active = true;
-
-              console.group(target);
-              console.log("entry:", entry);
-              console.log("rootBounds:", entry.rootBounds);
-              console.log("boundingClientRect:", entry.boundingClientRect);
-              console.groupEnd();
-
-              if (target.once) {
-                observer.unobserve(target);
-              }
-            } else {
-              target.active = false;
-            }
-          });
-        }, options);
-
-        IoFx.observers.set(observerKey, observer);
+          if (typeof parsed === "number" || Array.isArray(parsed)) {
+            thresholds = parsed;
+          } else {
+            throw new Error("Invalid threshold value");
+          }
+        } catch (e) {}
       }
 
-      this.observer = IoFx.observers.get(observerKey);
+      return Array.isArray(thresholds) ? thresholds : [thresholds];
     }
 
-    this.observer.observe(this);
+    get options() {
+      if (this.observer) {
+        const { root, rootMargin, thresholds: threshold } = this.observer;
+        return { root, rootMargin, threshold };
+      }
+
+      const { root, rootMargin, thresholds: threshold } = this;
+
+      return {
+        root,
+        rootMargin,
+        threshold,
+      };
+    }
+
+    connectedCallback() {
+      [
+        "duration",
+        "opacity-duration",
+        "transform-duration",
+        "delay",
+        "opacity-delay",
+        "transform-delay",
+        "easing",
+        "opacity-easing",
+        "transform-easing",
+      ].forEach((attr) => {
+        const attrValue = this.getAttribute(attr);
+
+        if (attrValue) {
+          this.style.setProperty(`--io-fx-${attr}`, attrValue);
+        }
+      });
+
+      if (!this.observer) {
+        const { options } = this;
+        const observerKey = JSON.stringify(options);
+
+        if (!IoFxElement.observers.has(observerKey)) {
+          const observer = new IntersectionObserver((entries, observer) => {
+            entries.forEach((entry) => {
+              const target = entry.target;
+
+              if (entry.isIntersecting) {
+                target.active = true;
+
+                if (target.once) {
+                  observer.unobserve(target);
+                }
+              } else {
+                target.active = false;
+              }
+            });
+          }, options);
+
+          IoFxElement.observers.set(observerKey, observer);
+        }
+
+        this.observer = IoFxElement.observers.get(observerKey);
+      }
+
+      this.observer.observe(this);
+    }
+
+    disconnectedCallback() {
+      this.observer.unobserve(this);
+    }
   }
 
-  disconnectedCallback() {
-    this.observer.unobserve(this);
+  if (!window.IoFxElement) {
+    window.IoFxElement = IoFxElement;
   }
-}
 
-if (!window.customElements.get("io-fx")) {
-  customElements.define("io-fx", IoFx);
-}
+  if (!window.customElements.get("io-fx")) {
+    customElements.define("io-fx", IoFxElement);
+  }
+})();
